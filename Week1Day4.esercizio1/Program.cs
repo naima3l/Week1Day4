c2978f289ae6c0c17c448306053ae8a37d34ba4e
naima.el.khattabi <naima.el.khattabi@avanade.com>
2021-07-15 11:24:29 +0200

    Add project files.

---
+++ Week1Day4.esercizio1/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Week1Day4.esercizio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Gioco del lancio dei Dadi
+
+
+
+            //Si chiede all'utente quanti dadi vuole lanciare.
+            //L'utente inserisce un numero per provare ad indovinare
+            //la somma dei valori usciti dal lancio dei dadi.
+            //Il computer lancia i dadi, quindi sorteggia i numeri(random tra 1 e 6).
+            //Se la somma dei due numeri random è pari al numero scelto dall'utente, l'utente vince
+            //Se l'utente vince, stampare 'hai vinto', altrimenti 'hai perso'.
+            //Finita la partita, l'utente deve poter rigiocare.
+            //Requisiti:
+            //Verificare che l'utente inserisca un intero e che sia compreso tra i valori possibili/accettabili.
+            //Se la verifica non va a buon fine, l'utente deve potere inserire nuovamente qualcosa.
+            //Opzionale->Creare un metodo per l'inserimento dei numeri e la verifica,
+            //uno per il controllo della vittoria(da chiamare nel main).
+
+            Menu();
+
+        }
+
+        private static void Menu()
+        {
+            Console.WriteLine("Benvenuto! Scegli l'opzione. \n1. Lancio dei dadi \n2. Exit");
+            int choice;
+            while (!int.TryParse(Console.ReadLine(), out choice))
+            {
+                Console.WriteLine("Scelta non valida. Devi inserire 1 per giocare o 2 per uscire. Riprova!");
+            }
+            Console.WriteLine($"{choice}");
+            if (choice == 1)
+            {
+                InserimentoEverifica();
+            }
+            else Console.WriteLine("Ciao ciao!");
+        }
+        private static void InserimentoEverifica()
+        {
+            Console.WriteLine("Quanti dadi vuoi tirare?");
+            int n;
+            while (!int.TryParse(Console.ReadLine(), out n))
+            {
+                Console.WriteLine("Scelta non valida. Devi inserire un numero itero. Riprova!");
+            }
+            Console.WriteLine("Ottimo! Adesso prova ad indovinare il numero");
+
+            int guess = 0;
+            while (!int.TryParse(Console.ReadLine(), out guess) || guess > (6*n))
+            {
+                Console.WriteLine($"Scelta non valida. Devi inserire un numero itero e non più grande di {(6*n)}. Riprova!");
+            }
+
+            Random random = new Random();
+            int[] val_dadi = new int[n];
+            int sum_dadi = 0;
+            for(int i = 0; i < n; i++)
+            {
+                val_dadi[i] = random.Next(1, 6);
+                sum_dadi += val_dadi[i];
+                Console.WriteLine($"{i+1}°Lancio : il dado ha restituito il numero {val_dadi[i]}");
+            }
+
+            
+            String answer = (guess == sum_dadi) ? "Bravissimo! Hai indovinato" : $"Mi dispiace, hai perso. La somma dei dadi era {sum_dadi}";
+            Console.WriteLine($"{answer}");
+            Menu();
+            
+
+        }
+    }
+}
---
+++ Week1Day4/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.IO;
+
+namespace Week1Day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Esempio1();
+
+            //Lettura e scrittura da file
+
+            //scrittura del file con chiusura manuale
+            StreamWriter sw = new StreamWriter(@"fileProva.txt"); //mi crea questo file nella cartella bean del progetto
+            sw.WriteLine("Ciao a tutte!");
+            sw.Close();
+
+            string path = @"C:\Users\naima.el.khattabi\source\repos\Week1Day4\Week1Day4\fileProva.txt";
+            StreamWriter sw1 = new StreamWriter(path);
+            sw1.WriteLine("Ciao a tutte!!!!!!");
+            sw1.Close();
+
+
+            ////scrittura del file con chiusura automatica -> using 
+            string path1 = @"C:\Users\naima.el.khattabi\source\repos\Week1Day4\Week1Day4\fileProva1.txt";
+
+            //using (StreamWriter sw2 =  new StreamWriter(path1))
+            //{
+            //    sw2.WriteLine("Bonjour!");
+            //}
+
+            ////Scrittura su file sovrascrivendo il contenuto precedente
+            //using(StreamWriter sw2 = new StreamWriter(path1))
+            //{
+            //    sw2.WriteLine("come state?");
+            //}
+
+            ////Scrittura su file mantendendo il contenuto precedente
+            //using(StreamWriter sw2 = new StreamWriter(path1,true))
+            //{
+            //    sw2.WriteLine("Bene, grazie!");
+            //}
+
+            //Lettura di tutto il file
+            using(StreamReader sw2 = new StreamReader(path1))
+            {
+                string contenutoFile = sw2.ReadToEnd();
+            }
+
+            //Lettura di una riga del file
+            using (StreamReader sw2 = new StreamReader(path1))
+            {
+                string contenutoRiga = sw2.ReadLine();
+            }
+
+            //Lettura di tutto il file e divisione del file in righe
+            using(StreamReader sw2 = new StreamReader(path1))
+            {
+                string contenutoFile = sw2.ReadToEnd();
+                string[] arrayRighe = contenutoFile.Split("\r\n");
+            }
+
+
+
+        }
+
+        private static void Esempio1()
+        {
+            //operatore ternario
+            int a = 1, b = 2;
+
+            string c;
+
+            if (a < b)
+            {
+                c = "a è più piccolo di b";
+            }
+            else
+            {
+                c = "a è più grande di b";
+            }
+
+            c = (a < b) ? "a è più piccolo di b" : "a è più grande di b";
+
+            //operatore ternario senza assegnazione
+
+            int pari = 0, dispari = 0, n = 10;
+
+            ((n % 2 == 0) ? ref pari : ref dispari)++;
+
+            //Resize array
+
+            int[] myArray = new int[10];
+            myArray[0] = 1;
+            myArray[1] = 2;
+            myArray[2] = 3;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"{myArray[i]} \t");
+            }
+
+            Console.WriteLine("\nStampa del mio array dopo il resize");
+            Array.Resize(ref myArray, 3);
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.Write($"{myArray[i]} \t");
+            }
+
+            Console.WriteLine("\n");
+            //foreach
+            foreach (var item in myArray)
+            {
+                Console.Write(item + "\t");
+            }
+        }
+    }
+}
